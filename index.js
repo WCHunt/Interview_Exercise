abc8680806e4343a98871accf1333a41a26f1ac0
WCHunt <wchunt@mail.csuchico.edu>
2022-04-08 18:47:47 -0700

    Updated index.js to return json object.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+
 const express = require("express")
-
 const app = express()
-
-app.get("/", (req,res)=> {
-    res.send("<h1>Test!</h1>")
-});
-
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.set('json spaces', 2)
+
+app.get("/", (req,res)=> {
+    const date = new Date()
+    const time = date.toLocaleTimeString("en-us",{timeZone: 'PST'})
+    const value = {message: 'My name is William', timestamp: time}
+    res.status(200).json(value)
+});
+
+app.listen(port, () => { console.log(`listening on port ${port}`)
+})
